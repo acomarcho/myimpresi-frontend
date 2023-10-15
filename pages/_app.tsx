41f88a2d1eeb9c135477bddba2aa12c56ea846b1
acomarcho <acoxstpd@gmail.com>
2023-10-15 13:18:40 +0700

    Feat: Initialize mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { Inter } from "next/font/google";
+import "@mantine/core/styles.css";
 
+import type { AppProps } from "next/app";
+import { MantineProvider, createTheme } from "@mantine/core";
+
+import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${inter.variable} font-inter`}>
-      <Component {...pageProps} />
+      <MantineProvider theme={theme}>
+        <Component {...pageProps} />
+      </MantineProvider>
     </main>
   );
 }
