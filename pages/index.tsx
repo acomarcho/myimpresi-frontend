2a8b3c6d479211de3262b0a0ff4fd88aa6430e36
acomarcho <acoxstpd@gmail.com>
2023-10-19 22:12:28 +0700

    Feat: Add menu for desktop view navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -105,14 +105,14 @@ const dummyProducts = [
   },
 ];
 
-import { Autocomplete } from "@mantine/core";
-import { IconSearch } from "@tabler/icons-react";
+import { Autocomplete, Menu } from "@mantine/core";
+import { IconSearch, IconCaretDownFilled } from "@tabler/icons-react";
 
 const Navbar = () => {
   return (
-    <div className="w-screen fixed top-0 left-0 z-[999] bg-white">
+    <div className="w-screen fixed top-0 left-0 z-[100] bg-white shadow-md">
       <div className="max-w-[1200px] mx-auto p-[1.5rem]">
-        <div className="grid grid-cols-[auto_1fr_auto] gap-[1rem] items-center">
+        <div className="grid grid-cols-[auto_1fr_auto] gap-[2rem] items-center">
           <Link href="/">
             <Image
               src="/assets/logo-impresi-blue.png"
@@ -130,6 +130,7 @@ const Navbar = () => {
                 paddingLeft: "2rem",
                 fontFamily: "var(--font-inter)",
                 color: "var(--neutral-100)",
+                fontSize: "1rem",
               },
             }}
             placeholder="Mau cari produk apa...?"
@@ -137,6 +138,59 @@ const Navbar = () => {
           <Link href="/" className="transition-all hover:scale-[1.05]">
             <Image src="/assets/heart.svg" width={28} height={32} alt="Love" />
           </Link>
+        </div>
+        <div className="flex gap-[2rem] mt-[1.5rem]">
+          <Link href="/" className="font-inter text-neutral-60 font-bold">
+            Home
+          </Link>
+          <Menu shadow="md" width={100}>
+            <Menu.Target>
+              <p className="font-inter text-neutral-60 font-bold cursor-pointer flex gap-[0.25rem] items-center">
+                Product <IconCaretDownFilled />
+              </p>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item>
+                <Link href="/" className="font-inter text-neutral-60 font-bold">
+                  Jam
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link href="/" className="font-inter text-neutral-60 font-bold">
+                  Pakaian
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link href="/" className="font-inter text-neutral-60 font-bold">
+                  Payung
+                </Link>
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+          <Menu shadow="md" width={100}>
+            <Menu.Target>
+              <p className="font-inter text-neutral-60 font-bold cursor-pointer flex gap-[0.25rem] items-center">
+                Events <IconCaretDownFilled />
+              </p>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item>
+                <Link href="/" className="font-inter text-neutral-60 font-bold">
+                  Seminar Kit
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link href="/" className="font-inter text-neutral-60 font-bold">
+                  Pameran Kit
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link href="/" className="font-inter text-neutral-60 font-bold">
+                  Rapat Formal
+                </Link>
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
         </div>
       </div>
     </div>
@@ -149,8 +203,8 @@ export default function Home() {
       <Head>
         <title>Impresi - Souvenir & Gifts</title>
       </Head>
-      <div className="relative">
-        <Navbar />
+      <Navbar />
+      <div className="relative pt-[8.5rem]">
         <div className="max-w-[1200px] mx-auto p-[1.5rem]">
           {/* Banner image */}
           <Banner />
