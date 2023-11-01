ee15710e634f572881486480fd0c9010a67b9297
acomarcho <acoxstpd@gmail.com>
2023-11-01 21:12:35 +0700

    Feat: Add dummy produk terlaris section

--- components/home/products.tsx
+++ components/home/products.tsx
@@ -72,48 +72,92 @@ const dummyProducts = [
 
 const Products = () => {
   return (
-    <div className="mt-[2rem] lg:mt-[5rem] mb-[2rem] lg:mb-[5rem]">
-      <Heading text="Produk Pilihan" />
-      <ScrollContainer className="flex flex-row gap-[1rem] lg:gap-[1.25rem] mt-[2rem] px-[0.5rem] pb-[1rem]">
-        {dummyProducts.map((p) => {
-          return (
-            <div
-              key={p.id}
-              className="bg-neutral-10 w-[50%] min-w-[180px] lg:w-[280px] flex-shrink-0 transition-all cursor-pointer hover:scale-[1.05] relative"
-            >
-              <Image
-                src={p.imagePath}
-                width={280}
-                height={280}
-                alt={p.name}
-                className="object-cover"
-              />
-              <div className="p-[0.75rem]">
-                <p className="font-inter font-bold">{p.name.toUpperCase()}</p>
-                <p className="font-inter text-neutral-60 text-[0.875rem] truncate-two">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Ipsum numquam reprehenderit et, earum deleniti nostrum tempore
-                  enim nihil corporis optio!
-                </p>
-                <div className="flex justify-between items-center mt-[0.5rem]">
-                  <p className="font-inter font-bold text-neutral-100 text-[0.9rem] lg:text-[1rem]">
-                    {formatToRupiah(p.price)}
+    <>
+      <div className="mt-[2rem] lg:mt-[5rem] mb-[2rem] lg:mb-[5rem]">
+        <Heading text="Produk Pilihan" />
+        <ScrollContainer className="flex flex-row gap-[1rem] lg:gap-[1.25rem] mt-[2rem] px-[0.5rem] pb-[1rem]">
+          {dummyProducts.map((p) => {
+            return (
+              <div
+                key={p.id}
+                className="bg-neutral-10 w-[50%] min-w-[180px] lg:w-[280px] flex-shrink-0 transition-all cursor-pointer hover:scale-[1.05] relative"
+              >
+                <Image
+                  src={p.imagePath}
+                  width={280}
+                  height={280}
+                  alt={p.name}
+                  className="object-cover"
+                />
+                <div className="p-[0.75rem]">
+                  <p className="font-inter font-bold">{p.name.toUpperCase()}</p>
+                  <p className="font-inter text-neutral-60 text-[0.875rem] truncate-two">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Ipsum numquam reprehenderit et, earum deleniti nostrum
+                    tempore enim nihil corporis optio!
                   </p>
-                  <button className="transition-all hover:scale-[1.2]">
-                    <Image
-                      src="/assets/heart.svg"
-                      width={16}
-                      height={16}
-                      alt="Add to wishlist"
-                    />
-                  </button>
+                  <div className="flex justify-between items-center mt-[0.5rem]">
+                    <p className="font-inter font-bold text-neutral-100 text-[0.9rem] lg:text-[1rem]">
+                      {formatToRupiah(p.price)}
+                    </p>
+                    <button className="transition-all hover:scale-[1.2]">
+                      <Image
+                        src="/assets/heart.svg"
+                        width={16}
+                        height={16}
+                        alt="Add to wishlist"
+                      />
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </ScrollContainer>
-    </div>
+            );
+          })}
+        </ScrollContainer>
+      </div>
+      <div className="mt-[2rem] lg:mt-[5rem] mb-[2rem] lg:mb-[5rem]">
+        <Heading text="Produk Terlaris" />
+        <ScrollContainer className="flex flex-row gap-[1rem] lg:gap-[1.25rem] mt-[2rem] px-[0.5rem] pb-[1rem]">
+          {dummyProducts.map((p) => {
+            return (
+              <div
+                key={p.id}
+                className="bg-neutral-10 w-[50%] min-w-[180px] lg:w-[280px] flex-shrink-0 transition-all cursor-pointer hover:scale-[1.05] relative"
+              >
+                <Image
+                  src={p.imagePath}
+                  width={280}
+                  height={280}
+                  alt={p.name}
+                  className="object-cover"
+                />
+                <div className="p-[0.75rem]">
+                  <p className="font-inter font-bold">{p.name.toUpperCase()}</p>
+                  <p className="font-inter text-neutral-60 text-[0.875rem] truncate-two">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Ipsum numquam reprehenderit et, earum deleniti nostrum
+                    tempore enim nihil corporis optio!
+                  </p>
+                  <div className="flex justify-between items-center mt-[0.5rem]">
+                    <p className="font-inter font-bold text-neutral-100 text-[0.9rem] lg:text-[1rem]">
+                      {formatToRupiah(p.price)}
+                    </p>
+                    <button className="transition-all hover:scale-[1.2]">
+                      <Image
+                        src="/assets/heart.svg"
+                        width={16}
+                        height={16}
+                        alt="Add to wishlist"
+                      />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollContainer>
+      </div>
+    </>
   );
 };
 
