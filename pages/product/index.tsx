dd54d3b4ca81e637fe8e2eafc8e7dde219731345
acomarcho <acoxstpd@gmail.com>
2023-11-05 18:55:42 +0700

    Feat: Add sample filter and pagination button

--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -2,8 +2,13 @@ import Head from "next/head";
 import Navbar from "@/components/common/navbar";
 import Heading from "@/components/home/heading";
 import CategoryScroll from "@/components/common/category-scroll";
+import { Select, Pagination } from "@mantine/core";
+import { useState } from "react";
 
 export default function Products() {
+  const [sortFilter, setSortFilter] = useState<string | null>("Rekomendasi");
+  const [activePage, setActivePage] = useState(1);
+
   return (
     <>
       <Head>
@@ -14,6 +19,33 @@ export default function Products() {
         <div className="max-w-[1200px] mx-auto p-[1.5rem]">
           <Heading text="Semua produk" />
           <CategoryScroll />
+          {/* Filter and pagination */}
+          <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center mt-[4rem] gap-[1rem]">
+            <div className="flex items-center gap-[1rem]">
+              <p className="font-inter font-bold">Urutkan: </p>
+              <Select
+                data={["Rekomendasi", "Harga Terendah", "Harga Tertinggi"]}
+                value={sortFilter}
+                onChange={setSortFilter}
+                styles={{
+                  input: {
+                    borderRadius: "100px",
+                  },
+                }}
+              />
+            </div>
+            <Pagination
+              value={activePage}
+              onChange={setActivePage}
+              total={10}
+              withControls={false}
+              styles={{
+                control: {
+                  border: "none",
+                },
+              }}
+            />
+          </div>
         </div>
       </div>
     </>
