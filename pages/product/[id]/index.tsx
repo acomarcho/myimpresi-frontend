dd893d09081e8b3de848cb049f3009f6495a6f5a
acomarcho <acoxstpd@gmail.com>
2023-11-07 09:58:38 +0700

    Refactor: Remove unused variable and imports

--- pages/product/[id]/index.tsx
+++ pages/product/[id]/index.tsx
@@ -2,10 +2,7 @@ import Head from "next/head";
 import Navbar from "@/components/common/navbar";
 import FloatingWAIcon from "@/components/common/floating-wa";
 import Footer from "@/components/common/footer";
-import Image from "next/image";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Heading from "@/components/home/heading";
-import CategoryScroll from "@/components/common/category-scroll";
 import ProductDetail from "@/components/product-detail/product-detail";
 import SimilarProducts from "@/components/product-detail/similar-products";
 import BestSellingProducts from "@/components/product-detail/best-selling-products";
@@ -31,73 +28,6 @@ const dummyProduct = {
   description:
     "Unik, stylish dan fungsional merupakan kesan yang ditampilkan oleh TAWIRI, tumbler premium ini cocok untuk anda yang menginginkan kreativitas dan dinamis menjadi kesan yang ditinggalkan oleh souvenir dari perusahaan / instansi anda.",
 };
-
-const dummyProducts = [
-  {
-    id: 1,
-    imagePath: "/dummy/produk.png",
-    name: "BANUM",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 2,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 3,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 4,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 5,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 6,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 7,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 8,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-];
 
 const SingleProduct = () => {
   return (
