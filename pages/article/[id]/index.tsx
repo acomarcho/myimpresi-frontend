cb0a2c082bcc5864903d2f953fa4f8dc1835f26a
acomarcho <acoxstpd@gmail.com>
2023-11-09 20:20:54 +0700

    Feat: Add artikel yang membantu

--- pages/article/[id]/index.tsx
+++ pages/article/[id]/index.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/common/navbar";
 import FloatingWAIcon from "@/components/common/floating-wa";
 import Footer from "@/components/common/footer";
 import Image from "next/image";
+import Link from "next/link";
 
 const dummyArticle = {
   id: 1,
@@ -62,7 +63,7 @@ const ArticleDetailPage = () => {
       <Navbar />
       <div className="relative pt-[8.5rem]">
         <div className="max-w-[1200px] mx-auto p-[1.5rem]">
-          <div className="flex flex-col">
+          <div className="flex flex-col gap-[2rem] lg:flex-row">
             <div>
               <h1 className="font-inter font-bold text-[1.5rem] text-neutral-100">
                 {article.title}
@@ -82,6 +83,34 @@ const ArticleDetailPage = () => {
                   </p>
                 );
               })}
+            </div>
+            <div className="lg:w-[400px] flex-shrink-0">
+              <div>
+                <h1 className="font-inter font-bold text-[1.25rem] text-neutral-100">
+                  Artikel yang membantu
+                </h1>
+                <div className="flex flex-col gap-[1rem] mt-[1rem]">
+                  {otherArticles.map((a) => {
+                    return (
+                      <Link
+                        href={`/article/${a.id}`}
+                        key={a.id}
+                        className="block grid grid-cols-2 gap-[1rem] transition-all hover:scale-[1.05]"
+                      >
+                        <Image
+                          src={a.imagePath}
+                          sizes="100%"
+                          height={0}
+                          width={0}
+                          className="w-full max-h-[100px] object-cover"
+                          alt={a.title}
+                        />
+                        <p className="font-inter font-bold">{a.title}</p>
+                      </Link>
+                    );
+                  })}
+                </div>
+              </div>
             </div>
           </div>
         </div>
