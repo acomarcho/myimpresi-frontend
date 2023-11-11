00b34cea108012a7e6f46f1507996a9a2826f16d
acomarcho <acoxstpd@gmail.com>
2023-11-11 14:54:13 +0700

    Feat: Wire product sliders at product detail apge

--- components/product-detail/best-selling-products.tsx
+++ components/product-detail/best-selling-products.tsx
@@ -1,80 +1,14 @@
 import ProductScroll from "../common/product-scroll";
 import Heading from "../home/heading";
-
-const dummyProducts = [
-  {
-    id: 1,
-    imagePath: "/dummy/produk.png",
-    name: "BANUM",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 2,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 3,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 4,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 5,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 6,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 7,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 8,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-];
+import usePromoProducts from "@/hooks/use-promo-products";
 
 const BestSellingProducts = () => {
-  const products = dummyProducts;
+  const { promoProducts } = usePromoProducts();
 
   return (
     <div className="mt-[2rem]">
       <Heading text="Produk Terlaris" />
-      <ProductScroll products={products} />
+      <ProductScroll products={promoProducts?.data ?? []} />
     </div>
   );
 };
--- components/product-detail/similar-products.tsx
+++ components/product-detail/similar-products.tsx
@@ -1,80 +1,14 @@
 import ProductScroll from "../common/product-scroll";
 import Heading from "../home/heading";
-
-const dummyProducts = [
-  {
-    id: 1,
-    imagePath: "/dummy/produk.png",
-    name: "BANUM",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 2,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 3,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 4,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 5,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 6,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 7,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-  {
-    id: 8,
-    imagePath: "/dummy/produk.png",
-    name: "HATIVE",
-    price: 120000,
-    minQuantity: 10,
-    soldAmount: 200,
-  },
-];
+import useFeaturedProducts from "@/hooks/use-featured-products";
 
 const SimilarProducts = () => {
-  const products = dummyProducts;
+  const { featuredProducts } = useFeaturedProducts();
 
   return (
     <div className="mt-[2rem]">
       <Heading text="Produk Serupa" />
-      <ProductScroll products={products} />
+      <ProductScroll products={featuredProducts?.data ?? []} />
     </div>
   );
 };
