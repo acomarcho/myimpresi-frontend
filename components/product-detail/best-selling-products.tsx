68d70b36a6b74758cb9fd04b828b41a25f137be3
acomarcho <acoxstpd@gmail.com>
2023-11-11 14:56:58 +0700

    Feat: Add shuffle on product detail 'Produk Terlaris' for randomness

--- components/product-detail/best-selling-products.tsx
+++ components/product-detail/best-selling-products.tsx
@@ -1,6 +1,7 @@
 import ProductScroll from "../common/product-scroll";
 import Heading from "../home/heading";
 import usePromoProducts from "@/hooks/use-promo-products";
+import _ from "lodash";
 
 const BestSellingProducts = () => {
   const { promoProducts } = usePromoProducts();
@@ -8,7 +9,9 @@ const BestSellingProducts = () => {
   return (
     <div className="mt-[2rem]">
       <Heading text="Produk Terlaris" />
-      <ProductScroll products={promoProducts?.data ?? []} />
+      <ProductScroll
+        products={(promoProducts?.data && _.shuffle(promoProducts.data)) ?? []}
+      />
     </div>
   );
 };
