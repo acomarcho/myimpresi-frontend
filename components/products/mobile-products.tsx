5dcaf320aafe4fa2f99a6873504ab40bc5662250
acomarcho <acoxstpd@gmail.com>
2023-11-11 20:42:04 +0700

    Feat: Add product not found page at mobile

--- components/products/mobile-products.tsx
+++ components/products/mobile-products.tsx
@@ -1,4 +1,4 @@
-import { IconAdjustments } from "@tabler/icons-react";
+import { IconAdjustments, IconZoomExclamation } from "@tabler/icons-react";
 import { formatToRupiah } from "@/utils/format-to-rupiah";
 import Link from "next/link";
 import Image from "next/image";
@@ -32,106 +32,124 @@ const MobileProducts = ({
       >
         <IconAdjustments />
       </button>
-      <div className="grid grid-cols-2 gap-[1rem] mt-[1rem]">
-        {products?.map((p) => {
-          return (
-            <Link
-              key={p.id}
-              className="bg-neutral-10 transition-all cursor-pointer hover:scale-[1.05] relative block"
-              href={`/product/${p.id}`}
-            >
-              <Image
-                src={p.productImage[0].path}
-                width={280}
-                height={280}
-                alt={p.name}
-                className="w-full h-[200px] lg:h-[250px] object-cover"
-              />
-              <div className="p-[0.75rem]">
-                <p className="font-inter font-bold">{p.name.toUpperCase()}</p>
-                <p className="font-inter text-neutral-60 text-[0.875rem] truncate-two h-[44px]">
-                  {`${p.material}, ${p.size}`}
-                </p>
-                <div className="flex justify-between items-center mt-[0.5rem]">
-                  <p className="font-inter font-bold text-neutral-100 text-[0.8rem] lg:text-[1rem]">
-                    {formatToRupiah(p.price)}
+      {products && products.length > 0 && (
+        <div className="grid grid-cols-2 gap-[1rem] mt-[1rem]">
+          {products?.map((p) => {
+            return (
+              <Link
+                key={p.id}
+                className="bg-neutral-10 transition-all cursor-pointer hover:scale-[1.05] relative block"
+                href={`/product/${p.id}`}
+              >
+                <Image
+                  src={p.productImage[0].path}
+                  width={280}
+                  height={280}
+                  alt={p.name}
+                  className="w-full h-[200px] lg:h-[250px] object-cover"
+                />
+                <div className="p-[0.75rem]">
+                  <p className="font-inter font-bold">{p.name.toUpperCase()}</p>
+                  <p className="font-inter text-neutral-60 text-[0.875rem] truncate-two h-[44px]">
+                    {`${p.material}, ${p.size}`}
                   </p>
-                  <button className="transition-all hover:scale-[1.2]">
-                    <Image
-                      src="/assets/heart.svg"
-                      width={16}
-                      height={16}
-                      alt="Add to wishlist"
-                    />
-                  </button>
+                  <div className="flex justify-between items-center mt-[0.5rem]">
+                    <p className="font-inter font-bold text-neutral-100 text-[0.8rem] lg:text-[1rem]">
+                      {formatToRupiah(p.price)}
+                    </p>
+                    <button className="transition-all hover:scale-[1.2]">
+                      <Image
+                        src="/assets/heart.svg"
+                        width={16}
+                        height={16}
+                        alt="Add to wishlist"
+                      />
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          );
-        })}
-        <Drawer opened={opened} onClose={close} position="bottom" size="80%">
-          <div className="flex flex-col">
-            <p className="font-inter font-bold text-neutral-100">
-              Semua Kategori
-            </p>
-            <hr className="text-neutral-20 mt-[0.5rem]" />
-            <Accordion
-              styles={{
-                control: {
-                  border: "none",
-                },
-                item: {
-                  border: "none",
-                },
-              }}
-            >
-              {filterData?.map((d) => {
-                return (
-                  <Accordion.Item key={d.id} value={d.name}>
-                    <Accordion.Control>
-                      <p
-                        className="font-bold font-inter text-neutral-100"
-                        style={{
-                          color: d.subcategory.find(
-                            (s) => s.id === filter.subcategoryId
-                          )
-                            ? colors.primaryDefault
-                            : "",
-                        }}
-                      >
-                        {d.name}
-                      </p>
-                    </Accordion.Control>
-                    <Accordion.Panel>
-                      <div className="flex flex-col gap-[1rem]">
-                        {d.subcategory.map((s) => {
-                          return (
-                            <button
-                              key={s.id}
-                              className="font-inter text-neutral-100 transition-all hover:pl-[0.5rem] cursor-pointer text-left"
-                              onClick={() => changeSubcategoryId(s.id)}
-                              style={{
-                                color:
-                                  filter.subcategoryId === s.id
-                                    ? colors.primaryDefault
-                                    : "",
-                                fontWeight:
-                                  filter.subcategoryId === s.id ? "bold" : "",
-                              }}
-                            >
-                              {s.name}
-                            </button>
-                          );
-                        })}
-                      </div>
-                    </Accordion.Panel>
-                  </Accordion.Item>
-                );
-              })}
-            </Accordion>
-          </div>
-        </Drawer>
-      </div>
+              </Link>
+            );
+          })}
+          <Drawer opened={opened} onClose={close} position="bottom" size="80%">
+            <div className="flex flex-col">
+              <p className="font-inter font-bold text-neutral-100">
+                Semua Kategori
+              </p>
+              <hr className="text-neutral-20 mt-[0.5rem]" />
+              <Accordion
+                styles={{
+                  control: {
+                    border: "none",
+                  },
+                  item: {
+                    border: "none",
+                  },
+                }}
+              >
+                {filterData?.map((d) => {
+                  return (
+                    <Accordion.Item key={d.id} value={d.name}>
+                      <Accordion.Control>
+                        <p
+                          className="font-bold font-inter text-neutral-100"
+                          style={{
+                            color: d.subcategory.find(
+                              (s) => s.id === filter.subcategoryId
+                            )
+                              ? colors.primaryDefault
+                              : "",
+                          }}
+                        >
+                          {d.name}
+                        </p>
+                      </Accordion.Control>
+                      <Accordion.Panel>
+                        <div className="flex flex-col gap-[1rem]">
+                          {d.subcategory.map((s) => {
+                            return (
+                              <button
+                                key={s.id}
+                                className="font-inter text-neutral-100 transition-all hover:pl-[0.5rem] cursor-pointer text-left"
+                                onClick={() => changeSubcategoryId(s.id)}
+                                style={{
+                                  color:
+                                    filter.subcategoryId === s.id
+                                      ? colors.primaryDefault
+                                      : "",
+                                  fontWeight:
+                                    filter.subcategoryId === s.id ? "bold" : "",
+                                }}
+                              >
+                                {s.name}
+                              </button>
+                            );
+                          })}
+                        </div>
+                      </Accordion.Panel>
+                    </Accordion.Item>
+                  );
+                })}
+              </Accordion>
+            </div>
+          </Drawer>
+        </div>
+      )}
+      {(!products || products.length === 0) && (
+        <div className="p-[1rem]">
+          <IconZoomExclamation size={128} color={colors.redIcon} />
+          <h1 className="font-inter font-bold text-[1.5rem] text-neutral-100 mt-[1rem]">
+            Hasil pencarian tidak ditemukan!
+          </h1>
+          <p className="font=inter text-neutral-60 mt-[1rem]">
+            Belum ada produk yang sesuai dengan pencarianmu. Coba ganti filter
+            atau kata kunci, atau hubungi admin untuk berkonsultasi terkait
+            produk yang dicari.
+          </p>
+          <button className="bg-primary-default px-[1rem] py-[0.5rem] font-bold font-inter text-neutral-10 mt-[1rem] transition-all hover:opacity-[0.9]">
+            Yuk, tanya admin!
+          </button>
+        </div>
+      )}
     </div>
   );
 };
