29a686850f0a0af26c679bec2dc81c0e19833888
acomarcho <acoxstpd@gmail.com>
2023-12-03 21:46:48 +0700

    Feat: Add proper loading skeleton for banner

--- components/home/banner.tsx
+++ components/home/banner.tsx
@@ -1,14 +1,17 @@
 import useBanners from "@/hooks/use-banners";
+import { Skeleton } from "@mantine/core";
 import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Scrollbar, Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
-import "swiper/css/scrollbar";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Autoplay, Navigation, Pagination, Scrollbar } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function Banner() {
-  const { banners } = useBanners();
+  const { banners, isLoading, error } = useBanners();
+
+  const shouldShowSkeleton = !banners || isLoading || !!error;
 
   return (
     <Swiper
@@ -22,6 +25,13 @@ export default function Banner() {
         disableOnInteraction: false,
       }}
     >
+      {shouldShowSkeleton && (
+        <SwiperSlide>
+          <Skeleton>
+            <Image src="/dummy/banner.jpg" width={1200} height={400} alt="" />
+          </Skeleton>
+        </SwiperSlide>
+      )}
       {banners?.banners.map((b) => {
         return (
           <SwiperSlide key={b.id}>
